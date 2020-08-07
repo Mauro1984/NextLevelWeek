7cfbbf5b50cbb784731313b42586adadde97b87c
Mauro Borges Junior <mauroborgesjunior@gmail.com>
2020-08-07 20:14:36 -0300

    Atualizaçao 9

--- src/server.js
+++ src/server.js
@@ -12,39 +12,40 @@ const proffys = [
     time_from: [720],
     time_to: [1400],
   },
-  {
-    name: "Mayk Brito",
-    avatar:
-      "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-    whatsapp: "111223456789",
-    bio:
-      "Um instrutor focado em ajudar as pessoas a começar a programar para a web - #html #css #javascript #sql #react #nodejs #fullstack.",
-    subject: "Matemática",
-    cost: "20",
-    weekday: "0",
-    time_from: [720],
-    time_to: [1400],
-  },
-  {
-    name: "Mayk Brito",
-    avatar:
-      "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-    whatsapp: "111223456789",
-    bio:
-      "Um instrutor focado em ajudar as pessoas a começar a programar para a web - #html #css #javascript #sql #react #nodejs #fullstack.",
-    subject: "Matemática",
-    cost: "20",
-    weekday: "0",
-    time_from: [720],
-    time_to: [1400],
-  },
 ];
+const subjects = [
+  "Artes",
+  "Biologia",
+  "Ciências",
+  "Educação física",
+  "Física",
+  "Geografia",
+  "História",
+  "Matemática",
+  "Português",
+  "Química",
+];
+const weekdays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+// funcionalidades
+function getSubject(subjectNumber) {
+  const position = +subjectNumber - 1;
+  return subjects[position];
+}
 
 const express = require("express");
 const server = express();
 
 //configurar nunjucks
 const nunjucks = require("nunjucks");
+const { query } = require("express");
 nunjucks.configure("src/views", {
   express: server,
   noCache: true,
@@ -58,10 +59,22 @@ server
     return res.render("index.html");
   })
   .get("/study", (req, res) => {
-    cost filters = req.query
-    return res.render("study.html", { proffys , filters});
+    const filters = req.query;
+    return res.render("study.html", { proffys, filters, subjects, weekdays });
   })
+
   .get("/give-classes", (req, res) => {
-    return res.render("give-classes.html");
+    const dados = req.query;
+    const isNotEmpty = Object.keys(dados).length > 0;
+    //se tiver dados
+    if (isNotEmpty) {
+      dados.subject = getSubject(dados.subject);
+      // adicionar a lista proffys
+      proffys.push(dados);
+      return res.redirect("/study");
+    }
+    // Se não, mostra a pagina
+
+    return res.render("give-classes.html", { subjects, weekdays });
   })
   .listen(5500);
